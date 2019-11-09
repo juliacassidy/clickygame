5e69de3b2b20f896c4508a28a0b55712ba00dc4c
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-11-09 10:28:55 -0500

    Updates

---
+++ precache-manifest.4bf1356e33755dbed022f75aff4d34b5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b457ff442758fdd9b84b6122ae52108",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "111153a89bb0054212ad",
+    "url": "/clickygame/static/css/main.c781a57d.chunk.css"
+  },
+  {
+    "revision": "fa10cc6c084e48b0ec90",
+    "url": "/clickygame/static/js/2.aac46861.chunk.js"
+  },
+  {
+    "revision": "111153a89bb0054212ad",
+    "url": "/clickygame/static/js/main.3aa58f89.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  },
+  {
+    "revision": "7d7c98f7bd43216fe2d24ebdd30a8fe7",
+    "url": "/clickygame/static/media/AZ.7d7c98f7.jpg"
+  },
+  {
+    "revision": "428884a965021d25e1bd2fe453ba794e",
+    "url": "/clickygame/static/media/KC.428884a9.jpg"
+  },
+  {
+    "revision": "a87c93d071c9496634df70ea874eaf82",
+    "url": "/clickygame/static/media/MIN.a87c93d0.jpg"
+  },
+  {
+    "revision": "0e18ee7b3ec8e512404d4bf9f6a5834a",
+    "url": "/clickygame/static/media/NO.0e18ee7b.png"
+  },
+  {
+    "revision": "08a1d28cc3fd0279527d9f9079d876ca",
+    "url": "/clickygame/static/media/PIT.08a1d28c.jpg"
+  },
+  {
+    "revision": "64fbcdd41db0769832cc14729bdb40ef",
+    "url": "/clickygame/static/media/dal.64fbcdd4.png"
+  },
+  {
+    "revision": "309a2bd8f9484bc6feec1665b8853a7f",
+    "url": "/clickygame/static/media/gb.309a2bd8.jpg"
+  },
+  {
+    "revision": "0b9ae383033bafa384193864bf0ba5ef",
+    "url": "/clickygame/static/media/lion.0b9ae383.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clickygame/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1e4c92714b298e428ab9d9180d71e36f",
+    "url": "/clickygame/static/media/philly.1e4c9271.png"
+  },
+  {
+    "revision": "8be425d77ed6d0513a10453289137f8e",
+    "url": "/clickygame/static/media/rams.8be425d7.png"
+  },
+  {
+    "revision": "eabe6f2d0dfd8621c1b758c0117573c2",
+    "url": "/clickygame/static/media/tex.eabe6f2d.png"
+  }
+]);
